d00dd9b69218173542971f4684ebeecfa3aac157
Samay Pant <pantsamay@gmail.com>
2025-04-23 14:25:56 +0530

    Fix: added missing import for os

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
+import os  # <-- This line is crucial!
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # This looks inside the templates folder
+    return render_template('index.html')
 
 if __name__ == '__main__':
-    import os
-port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=port)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
