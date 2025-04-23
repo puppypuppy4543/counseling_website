743617893da35e984c0a5a13d6b656808dfc0b42
Samay Pant <pantsamay@gmail.com>
2025-04-23 13:13:35 +0530

    fix: dynamic port for Render

--- app.py
+++ app.py
@@ -7,4 +7,7 @@ def home():
     return render_template('index.html')  # This looks inside the templates folder
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host='0.0.0.0', port=port)
+
